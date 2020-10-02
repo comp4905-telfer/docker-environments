cdd6145d7b9bbbe3471745897f770c4ca5fb1efb
Andre Telfer <telfer006@gmail.com>
2020-10-01 21:55:48 -0400

    name notebooks by server

--- notebook-env/tf1-dlc-darknet/run.sh
+++ notebook-env/tf1-dlc-darknet/run.sh
@@ -1,2 +1,2 @@
-jupyter lab build --name=$NOTEBOOK_TITLE
+jupyter lab build --name=$NOTEBOOK_TITLE_PREFIX-$NOTEBOOK_TITLE_POSTFIX
 jupyter lab --ip 0.0.0.0 --port 8888 --no-browser --allow-root --NotebookApp.token=$NOTEBOOK_PASSWORD --NotebookApp.allow_origin=* --NotebookApp.notebook_dir=/
